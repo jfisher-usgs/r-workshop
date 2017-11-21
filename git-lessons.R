45b7a518d20eef3011ef610e9461931c0af39db5
Jason Fisher <jfisher@usgs.gov>
2017-11-21 15:20:09 -0800

    Render site

--- git-lessons.R
+++ git-lessons.R
@@ -1,28 +1,18 @@
-## ---- engine="bash", eval=FALSE------------------------------------------
 ## git config --global user.name "<YOUR NAME>"
 
-## ---- engine="bash", eval=FALSE------------------------------------------
 ## git config --global user.email <YOUR EMAIL ADDRESS>
 
-## ---- engine="bash", eval=FALSE------------------------------------------
 ## git init
 
-## ---- engine="bash", eval=FALSE------------------------------------------
 ## git add .
 
-## ---- engine="bash", eval=FALSE------------------------------------------
 ## git add repo-example/test/file2.txt
 
-## ---- engine="bash", eval=FALSE------------------------------------------
 ## git state
 
-## ---- engine="bash", eval=FALSE------------------------------------------
 ## git commit -m "Commit message"
 
-## ---- engine="bash", eval=FALSE------------------------------------------
 ## git remote add origin git@github.com:jfisher-usgs/repo-example.git
-## git push -u origin master
 
-## ---- engine="bash", eval=FALSE------------------------------------------
 ## git push
 
