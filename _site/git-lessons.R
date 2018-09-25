890b6181b5a3e340950cad04e6905a7afb70807f
Jason Fisher <jfisher@usgs.gov>
2018-09-25 08:11:48 -0700

    Re-render site

---
+++ _site/git-lessons.R
@@ -0,0 +1,18 @@
+## git config --global user.name "<YOUR NAME>"
+
+## git config --global user.email <YOUR EMAIL ADDRESS>
+
+## git init
+
+## git add .
+
+## git add repo-example/test/file2.txt
+
+## git state
+
+## git commit -m "Commit message"
+
+## git remote add origin git@github.com:jfisher-usgs/repo-example.git
+
+## git push
+
